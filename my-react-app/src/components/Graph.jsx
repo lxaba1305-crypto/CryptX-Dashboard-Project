8c928195ad4a2c5261749a76bb1dc2675f4e1272
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-02-25 18:36:17 +0200

    Began adding code to the Graphs section

---
+++ my-react-app/src/components/Graph.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Graph = () => {
+    return (
+        <div className="bg-white rounded-xl shadow-lg p-6 flex flex-col">
+
+            {/* Header */}
+            <div className="flex justify-between items-center mb-4">
+                <h2 className="text-xl font-bold">BTC Prices</h2>
+
+                <div className="flex space-x-2">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
