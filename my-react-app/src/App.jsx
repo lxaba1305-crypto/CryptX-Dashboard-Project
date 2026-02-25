2a77e57fc912e51db0a783921d688ac7f4437a60
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-02-25 14:10:55 +0200

    Added Tailwind to the CSS, and the sections to the App.jsz

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import Sidebar from './components/Sidebar.jsx'
+import TopNavbar from './components/TopNavbar.jsx'
+import SummaryCards from './components/SummaryCards.jsx'
+import GraphCard from './components/GraphCard.jsx'
+import LiveMarket from './components/LiveMarket.jsx'
+import Transactions from './components/Transactions.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex-1 flex flex-col bg-gray-100">
+          <TopNavbar />
+          <div className="p-6">
+            <SummaryCards />
+            </div>
+            <div className="p-6">
+            <GraphCard />
+            </div>
+            <div className="grid md:grid-cols-2 gap-6 p-6">
+            <LiveMarket />
+            <Transactions />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
 }
 
 export default App
